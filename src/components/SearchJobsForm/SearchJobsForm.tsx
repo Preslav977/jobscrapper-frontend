0d8b09c32a4fe8a71a5aeb389f74cee46d12f1b9
Preslav977 <p.cvetanow@gmail.com>
2026-05-11 11:06:45 +0300

    Applied CSS on SearchJobsForm

--- src/components/SearchJobsForm/SearchJobsForm.tsx
+++ src/components/SearchJobsForm/SearchJobsForm.tsx
@@ -1,18 +1,22 @@
 import type { SearchJobsFormInterface } from "../../interfaces/SearchJobsFormInterface/SearchJobsFormInterface";
+import styles from "./SearchJobsForm.module.css";
 
 export function SearchJobsForm({ onSubmit }: SearchJobsFormInterface) {
   return (
-    <>
+    <div className={styles.formWrapper}>
       <form
+        className={styles.formContainer}
         onSubmit={(e) => {
           e.preventDefault();
           void onSubmit(e);
         }}
       >
         <label htmlFor="query"></label>
-        <input type="text" name="query" placeholder="Search jobs..." />
-        <button type="submit">Search</button>
+        <div className={styles.formContent}>
+          <input type="text" name="query" placeholder="Search jobs..." />
+          <button type="submit">Search</button>
+        </div>
       </form>
-    </>
+    </div>
   );
 }
