60e4b4b1a90dee3b0501644be17303b05e82227f
Preslav977 <p.cvetanow@gmail.com>
2026-05-12 09:48:35 +0300

    Created custom hook for fetching jobs details, and component to render it

---
+++ src/custom hooks/useFetchJobsDetails/useFetchJobsDetails.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import type { Jobs } from "../../interfaces/CompanyJobsInterface/CompanyJobsInterface";
+import { localhostURL } from "../../utility/localhostURL";
+
+async function fetchJobsDetails(id: number): Promise<Jobs[]> {
+  const response = await fetch(`${localhostURL}/companies/jobs/${id}`, {
+    mode: "cors",
+    headers: {
+      Authorization: sessionStorage.getItem("token")!,
+    },
+  });
+
+  return response.json() as Promise<Jobs[]>;
+}
+
+export const useFetchJobsDetails = (id: number) => {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["jobsDetails", id],
+    queryFn: () => fetchJobsDetails(id),
+    initialData: [],
+  });
+
+  return { isPending, isError, data, error };
+};
