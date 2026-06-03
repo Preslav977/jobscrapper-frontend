62943736fc7bb846874291b3cdab724ade09cfe5
Preslav977 <p.cvetanow@gmail.com>
2026-06-03 10:38:07 +0300

    Test to render erros if the inputs are empty when submitting

--- src/tests/LogInForm/LogInForm.test.tsx
+++ src/tests/LogInForm/LogInForm.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import { renderRouter } from "../../router/renderRouter";
 
@@ -25,5 +26,29 @@ describe("render LoginForm", () => {
     );
 
     expect(screen.queryByText("Sign up")?.textContent).toMatch(/sign up/i);
+  });
+
+  it("should render errors if the inputs are empty when submitting", async () => {
+    renderRouter({ initialEntries: ["/login"] });
+
+    const user = userEvent.setup();
+
+    const logInButton = screen.queryByRole("button", { name: "Log in" });
+
+    await user.click(logInButton!);
+
+    // screen.debug();
+
+    expect(screen.queryByText("Must be valid email!")?.textContent).toMatch(
+      /must be valid email!/i,
+    );
+
+    expect(
+      screen.queryByText(
+        "Password must be minimum 8 characters, and contain at least one letter, and one number",
+      )?.textContent,
+    ).toMatch(
+      /password must be minimum 8 characters, and contain at least one letter, and one number/i,
+    );
   });
 });
