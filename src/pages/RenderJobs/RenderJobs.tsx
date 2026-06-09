9312db07f0eda7d27165a5f9f57382a153ddb062
Preslav977 <p.cvetanow@gmail.com>
2026-06-09 10:17:18 +0300

    Made RenderJobs responsive, added loading svg and magnifying glass svg

--- src/pages/RenderJobs/RenderJobs.tsx
+++ src/pages/RenderJobs/RenderJobs.tsx
@@ -9,40 +9,57 @@ export function RenderJobs({
   isPending,
   error,
 }: RenderJobsInterface) {
-  if (isPending) return <LoadingComponent loading={"Loading..."} />;
+  if (isPending)
+    return (
+      <div className={styles.loadingJobsContainer}>
+        <img className="loading" src="./loading.svg" alt="Loading" />
+        <LoadingComponent loading={"Loading jobs, please wait..."} />
+      </div>
+    );
 
   if (error) return <ErrorComponent error={error} />;
 
   return (
-    <div className={styles.jobsWrapper}>
-      {filteredJobs.map((jobs) => (
-        <div key={jobs.id}>
-          <div className={styles.jobsContainer}>
-            <Link to={`/jobs/${jobs.id}`}>
-              <img
-                className={styles.jobsCompanyLogo}
-                src={jobs.company.logo ? jobs.company.logo : "Companies logos"}
-                alt="Company Logo"
-              />
-              <h2 className={styles.jobsTitle}>{jobs.title}</h2>
-              <div className={styles.jobsDescriptionWrapper}>
-                <p>{jobs.location}</p>
-                <p>{jobs.remoteOrHybrid}</p>
-                <p>{jobs.datePosted}</p>
-              </div>
-            </Link>
+    <>
+      <div className={styles.jobsWrapper}>
+        {filteredJobs.map((jobs) => (
+          <div key={jobs.id}>
+            <div className={styles.jobsContainer}>
+              <Link to={`/jobs/${jobs.id}`}>
+                <img
+                  className={styles.jobsCompanyLogo}
+                  src={
+                    jobs.company.logo
+                      ? jobs.company.logo
+                      : `${jobs.company.name} Logo`
+                  }
+                  alt={jobs.company.name + "Logo"}
+                />
+                <h2 className={styles.jobsTitle}>{jobs.title}</h2>
+                <div className={styles.jobsDescriptionWrapper}>
+                  <p>{jobs.location}</p>
+                  <p>{jobs.remoteOrHybrid}</p>
+                  <p>{jobs.datePosted}</p>
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
-      {filteredJobs.length === 0 && (
-        <div className={styles.noJobsContainer}>
-          <p className={styles.noJobsFoundPara}>No jobs found</p>
-          <p className={styles.noJobsFoundExplainingPara}>
-            We couldn't find any jobs that match your search. Try adjusting your
-            filters or check back later.
-          </p>
-        </div>
-      )}
-    </div>
+        ))}
+        {filteredJobs.length === 0 && (
+          <div className={styles.noJobsContainer}>
+            <img
+              className={styles.noJobsFoundSVG}
+              src="./magnifying-glass.svg"
+              alt="No jobs has been found"
+            />
+            <p className={styles.noJobsFoundPara}>No jobs found</p>
+            <p className={styles.noJobsFoundExplainingPara}>
+              We couldn't find any jobs that match your search. Try adjusting
+              your filters or check back later.
+            </p>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
