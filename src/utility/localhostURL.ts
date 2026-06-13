a1d543530f9fc8f7600e866fb0d7d470b4b0e7d6
Preslav977 <p.cvetanow@gmail.com>
2026-06-13 10:35:33 +0300

    Swapped the localhostURL with the deployed backend URL

--- src/utility/localhostURL.ts
+++ src/utility/localhostURL.ts
@@ -1,1 +1,3 @@
-export const localhostURL = "http://localhost:5000";
+// export const localhostURL = "http://localhost:5000";
+
+export const localhostURL = "https://jobscrapper-backend-vhpo.onrender.com";
